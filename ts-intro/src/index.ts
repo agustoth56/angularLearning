925a4be9f0f0a19c643fcd62788b465b53960a8c
Agustin Toth <agustoth56@gmail.com>
2021-07-23 11:52:25 +1200

    modificando index.ts

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,15 @@
 
-
-console.log('Hola Mundo!!!!');
-
 /*
     ===== Código de TypeScript =====
 */
+
+let nombre: string = "Nombre";
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+nombre = "Agustin";
+hp = "FULL";
+
+
+console.log(nombre, hp);
 
