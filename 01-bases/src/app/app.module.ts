00f512f87fcec208170b1c01e83250ef67a02166
Agustin Toth <agustoth56@gmail.com>
2021-08-04 17:04:50 +1200

    cambiando nombres por boludo

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heores/heore/heore.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
