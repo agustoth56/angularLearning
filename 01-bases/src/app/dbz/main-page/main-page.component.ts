ca4e6e005ebb04d2a0716c901018866a821ef5dc
Agustin Toth <agustoth56@gmail.com>
2021-08-04 23:47:21 +1200

    update

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -21,22 +21,8 @@ export class MainPageComponent {
   ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder : 0
-  }
-
-  agregar(){
-    
-    if (this.nuevo.nombre.trim().length === 0){
-      return;
-    }
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder : 0
-    }
-
+    nombre: 'pepe',
+    poder : 1000
   }
 
 }
